993f51dcf6313ff139903f00802ea9fa39c12c77
mgkEdits <elijahnyasiando.mn@gmail.com>
2023-12-19 07:39:26 +0300

    cmplt lab

---
+++ app/migrations/versions/65b6a2822b43_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: 65b6a2822b43
+Revises: 
+Create Date: 2023-12-19 07:34:26.262550
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '65b6a2822b43'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
